c4b150ddaaa8dec8cf1f7978cb0124add84b0fa8
spcrcwater <89970914+spcrcwater@users.noreply.github.com>
2021-10-31 03:37:35 +0530

    new_update_

--- run_cmd_bash.sh
+++ run_cmd_bash.sh
@@ -3,14 +3,17 @@
 wget -q --spider http://google.com
 
 cicomid=$(git rev-parse HEAD)
+cicomid=${cicomid:0:7}
+
 if [[ $? -eq 0 ]]
  then
     cd /home/pi/Desktop/waterspcrc/Ph-03/
     git pull origin main
     cicomid_new=$(git rev-parse HEAD)
-    if [[ cicomid -ne cicomid_new ]]
+    cicomid_new=${cicomid_new:0:7}
+    if [[ "$cicomid" != "$cicomid_new" ]]
     then
-      cicomid = cicomid_new
+      cicomid=$cicomid_new
       sudo systemctl restart codetest.service
     else
       echo 'No changes'
@@ -18,4 +21,5 @@ if [[ $? -eq 0 ]]
 
 else
     echo 'No Internet'
+
 fi
