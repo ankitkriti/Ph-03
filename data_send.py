6b009f026b03478add916544b414db9a3f4cd7a6
Ankit Kriti <61308451+ankitkriti@users.noreply.github.com>
2025-07-11 19:29:38 +0530

    Update data_send.py merged v1

--- data_send.py
+++ data_send.py
@@ -1,5 +1,4 @@
 import time
-import random
 import requests
 
 # ====== OneM2M Configuration ======
@@ -9,11 +8,11 @@ HTTPS = False
 OM2M_ORIGIN = "Tue_20_12_22:Tue_20_12_22"
 OM2M_MN = "/~/in-cse/in-name/"
 OM2M_AE = "AE-WM/WM-WF"
-OM2M_DATA_CONT = "WM-WF-PL00-70/Data"
+# OM2M_DATA_CONT = "WM-WF-PL00-70/Data"
 
 # ====== ThingSpeak Configuration ======
-THINGSPEAK_CHANNEL = "3004930"
-THINGSPEAK_WRITE_API = "8PBJAK3JCNTB3NH7"
+# THINGSPEAK_CHANNEL = "3004930"
+# THINGSPEAK_WRITE_API = "8PBJAK3JCNTB3NH7"
 
 # ====== Telegram Bot Configuration ======
 TELEGRAM_BOT_TOKEN = "8080932314:AAEss9FRi7hx-zBakJeyl06jb7caY2j9bIk"
@@ -24,7 +23,7 @@ TELEGRAM_CHAT_ID = "879323820"
 def get_epoch_time():
     return int(time.time())
 
-def send_onem2m_data(timestamp, total_flow, flow_rate):
+def send_onem2m_data(timestamp, total_flow, flow_rate, OM2M_DATA_CONT):
     data = f"[{timestamp}, {total_flow}, {flow_rate}]"
     protocol = "https" if HTTPS else "http"
     url = f"{protocol}://{CSE_IP}:{CSE_PORT}{OM2M_MN}{OM2M_AE}/{OM2M_DATA_CONT}"
@@ -54,13 +53,12 @@ def send_onem2m_data(timestamp, total_flow, flow_rate):
         if 'response' in locals():
             response.close()
 
-def send_thingspeak_data(timestamp, total_flow, flow_rate):
+def send_thingspeak_data(total_flow, flow_rate, THINGSPEAK_WRITE_API):
     url = "https://api.thingspeak.com/update"
     params = {
         "api_key": THINGSPEAK_WRITE_API,
-        "field1": timestamp,
-        "field2": total_flow,
-        "field3": flow_rate
+        "field1": total_flow,
+        "field2": flow_rate
     }
 
     print("\n Sending to ThingSpeak...")
