adf1a7b099a8bdb3819ed00116e2f23fae088bf8
Ankit Kriti <61308451+ankitkriti@users.noreply.github.com>
2025-07-14 11:36:22 +0530

    Update codetest.py camera enabled

--- codetest.py
+++ codetest.py
@@ -185,10 +185,10 @@ def wait():
 
 
 def cam(save_path):
-    # camera = PiCamera ()
-    # camera.start_preview ()
+    camera = PiCamera ()
+    camera.start_preview ()
     time.sleep (3)
-    # camera.capture (save_path)
+    camera.capture (save_path)
     print ('Captured')
 
 
@@ -198,8 +198,8 @@ def cam(save_path):
     camera_error.append(error_log5)
 
 
-    # camera.stop_preview ()
-    # camera.close ()
+    camera.stop_preview ()
+    camera.close ()
 
 
     error_log6 = []
@@ -256,8 +256,8 @@ def get_sorted_contour(img):
 def func(save_path, Filename, error_log3, error_log4, error_log5, error_log6):
     count_internet = 0
     global cons
-    # img = sio.imread (save_path)
-    img = sio.imread("/home/pi/Desktop/images/img2022-01-24-13-53-39.jpg")
+    img = sio.imread (save_path)
+    # img = sio.imread("/home/pi/Desktop/images/img2022-01-24-13-53-39.jpg")
     # img = sio.imread("/home/pi/Desktop/images/img2021-08-05-22-52-56.jpg")
     # plt.imshow(img)
     # plt.show()
@@ -586,7 +586,7 @@ def main():
 
             t2 = int (time.time ())
             # wait()
-            delay = 120 - (t2 - t1) #capture after 5 mins
+            delay = 300 - (t2 - t1) #capture after 5 mins
             if delay <= 0:
                 delay = 0.5
             print (delay)
